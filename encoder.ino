fe98981a6cf6f2d0ccdcd12be3905c7f0081d410
Daniel Kuhn <55718508+DanielUmedaKuhn@users.noreply.github.com>
2025-07-15 18:51:48 +0200

    Update encoder.ino
    Added remote topic publishing

--- encoder.ino
+++ encoder.ino
@@ -1,31 +1,105 @@
-const int encoderPin = 8; 
-const int ledPin = 6; 
+#include <WiFi.h>
+#include "esp_wpa2.h"
+#include <PubSubClient.h>
+
+//Wifi Credentials
+const char* ssid = "HAWHof";  
+const char* eap_identity = "dumedakuhn";    //Username for the network
+const char* eap_password = "Trebuchets90300.";  //Password for the network
+
+//MQTT Configs
+const char* mqtt_server = "broker.hivemq.com";
+const char* status_topic = "nano-esp32/makerspace/status";   //Name of the topic to be subscribed in
+const char* client_id = "arduino-nano-esp32-inactivity-tracker";
+
+//Code variables
+const int encoderPin = 8;     //Pin to be connected to the encoder
+const int ledPin = 6;         
 unsigned long lastDetectionTime = 0;
-const unsigned long inactivityTimeout = 300000;    //value should be in milliseconds, 300000 = 5min
-bool wasActive = false;    //Tracks the  encoder state, true = time since last state change < inactivityTimeout, false = > inactivityTimeout
+const unsigned long inactivityTimeout = 300000;   //value should be in milliseconds, 300000 = 5min
+bool wasActive = false; //Flag to track the  encoder state, true = time since last state change < inactivityTimeout, false = > inactivityTimeout
+
+//Client Objects
+WiFiClient espClient;
+PubSubClient client(espClient);
+
+
+//Interruption
+void IRAM_ATTR encoderTriggered() {
+  lastDetectionTime = millis();
+  digitalWrite(ledPin, LOW);   
+  wasActive = true;
+}
+
+//Connection to Enterprise Wifi (University Wifi)
+void setup_wifi_enterprise() {
+  Serial.println("Configuring Wi-Fi to WPA2-Enterprise...");
+  WiFi.disconnect(true);
+  WiFi.mode(WIFI_STA);
+  esp_wifi_sta_wpa2_ent_enable();
+  esp_wifi_sta_wpa2_ent_set_identity((uint8_t *)eap_identity, strlen(eap_identity));
+  esp_wifi_sta_wpa2_ent_set_username((uint8_t *)eap_identity, strlen(eap_identity));
+  esp_wifi_sta_wpa2_ent_set_password((uint8_t *)eap_password, strlen(eap_password));
+  WiFi.begin(ssid);
+  Serial.print("Attempting to connect to network: ");
+  Serial.println(ssid);
+  int attempts = 0;
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+    if (++attempts > 30) {
+      Serial.println("\nFailed to connect.");
+      return;
+    }
+  }
+  Serial.println("\nWiFi connected successfully.");
+}
+
+//MQTT Reconnection Function
+void reconnect() {
+  while (!client.connected()) {
+    Serial.print("Attempting to connect to MQTT Broker...");
+    if (client.connect(client_id)) {
+      Serial.println("Connected.");
+    } else {
+      Serial.print("failed, rc=");
+      Serial.print(client.state());
+      Serial.println(" attempting again in 5 seconds");
+      delay(5000);
+    }
+  }
+}
 
 void setup() {
-  Serial.begin(9600);
-  pinMode(encoderPin, INPUT_PULLUP);
-  pinMode(ledPin, OUTPUT); 
+  Serial.begin(115200);
+  pinMode(ledPin, OUTPUT);
+  pinMode(encoderPin, INPUT_PULLUP); 
+  digitalWrite(ledPin, LOW); 
   attachInterrupt(digitalPinToInterrupt(encoderPin), encoderTriggered, CHANGE);  //Launch the interruption every time the encoder state changes
-  lastDetectionTime = millis();
+
+  //Wif and MQTT connection
+  setup_wifi_enterprise();
+  client.setServer(mqtt_server, 1883);
+  lastDetectionTime = millis(); //Starts the timer
+  Serial.println("Setup concluded. Monitoring Activity...");
 }
 
 void loop() {
-  if(millis() - lastDetectionTime > inactivityTimeout){    //Maximum waiting threshold exceeded
-    if(wasActive){     //Prevents light from turning on when first powering on the system, as well as overwriting when the light is already on
-      Serial.println("No rotation detected");
-      digitalWrite(ledPin, HIGH);         //Light goes on
-      wasActive = false;    //reset
+  //Maintain connection with Wifi and the MQTT Broker
+  if (WiFi.status() != WL_CONNECTED) return;
+  if (!client.connected()) {
+    reconnect();
+  }
+  client.loop();
+
+//Inactivity Verification
+  if (millis() - lastDetectionTime > inactivityTimeout) {    //Maximum waiting threshold exceeded
+    if (wasActive) {      //Prevents light from turning on when first powering on the system, as well as repeated publication      
+      Serial.println("Inactivity detected! Turning LED on and publishing to MQTT.");
+      digitalWrite(ledPin, HIGH);
+      client.publish(status_topic, "Inactivity detected: LED on.");    //Message to be published to MQTT topic
+
+      wasActive = false; //Reset the flag
     }
   }
-  delay(10);
-}
-
-//Interruption
-void encoderTriggered(){        
-  lastDetectionTime = millis();  //Reset timer
-  digitalWrite(ledPin, LOW);    //Light goes off
-  wasActive = true;
 }
